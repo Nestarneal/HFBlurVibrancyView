dcaf35880a90b19a3318792500d4d274490b8394
Nestarneal <nestarneal@gmail.com>
2018-07-01 09:49:36 +0800

    Initial Commit

---
+++ HFBlurVibrancyView/HFBlurVibrancyView.h
@@ -0,0 +1,19 @@
+//
+//  HFBlurVibrancyView.h
+//  HFBlurVibrancyView
+//
+//  Created by Nestarneal on 2018/07/01.
+//  Copyright © 2018年 Han-Feng Lin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HFBlurVibrancyView.
+FOUNDATION_EXPORT double HFBlurVibrancyViewVersionNumber;
+
+//! Project version string for HFBlurVibrancyView.
+FOUNDATION_EXPORT const unsigned char HFBlurVibrancyViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HFBlurVibrancyView/PublicHeader.h>
+
+
